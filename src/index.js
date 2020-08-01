82b625c7ea9e29fa1ba6e82934f845b6242d3c2d
Aleksander <Zakharov.owl@gmail.com>
2020-08-01 18:43:25 +0300

    webpack 01.08

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-import Post from './Post'
+import * as $ from 'jquery'
+import Post from './Post.js'
+import json from './assets/json.json'
+import xml from './assets/data.xml'
+import csv from './assets/data.csv'
+import WebpackLogo from './assets/webpack-logo.png'
 import './styles/styles.css'
 
-const post = new Post('Webpack Post Title ')
+const post = new Post('Webpack Post Title', WebpackLogo)
+
+$('pre').addClass('code').html(post.toString())
 
 console.log('Post to String', post.toString());
+
+console.log('JSON:', json);
+console.log('XML:', xml);
+console.log('CSV:', csv);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -13,17 +14,53 @@ module.exports = {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist')
     },
+    resolve: {
+        extensions: ['.js','.json','.png'],
+        alias: {
+            '@':path.resolve(__dirname,'src')
+        }
+    }, 
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+    devServer: {
+        port: 4200
+    },
     plugins: [
         new HTMLWebpackPlugin({
             template: './index.html'
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin([
+            {
+                from: path.resolve(__dirname,'src/favicon.ico'),
+                to: path.resolve(__dirname,'dist')
+            }
+        ])
     ],
     module: {
         rules: [
             {
                 test: /\.css$/,
                 use: ['style-loader','css-loader']
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.xml$/,
+                use: ['xml-loader']
+            },
+            {
+                test: /\.csv$/,
+                use: ['csv-loader']
             }
         ]
     },
